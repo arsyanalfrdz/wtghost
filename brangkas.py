0da112a11f73ffa5e44a22693489123a4823fe5b
arsyanalfrdz <arsyanalfrdz@gmail.com>
2026-06-13 21:04:19 +0700

    Update brangkas.py

--- brangkas.py
+++ brangkas.py
@@ -559,7 +559,7 @@ async def bantuan(ctx):
 # 4. MENJALANKAN BOT DAN WEBSITE
 # ==========================================
 if __name__ == '__main__':
-    if not TOKEN or TOKEN == "MTUxMzU0NTUxMjU4OTIwMTU0Mg.GWcppL.urSXyEkkRpWWYSNXwT4Z4QBymt5MZpDbiJXq-U":
+    if not TOKEN or TOKEN == "":
         print("❌ ERROR: Token Discord belum diatur di file .env!")
     else:
         # Jalankan Flask Server di background (Thread baru)
